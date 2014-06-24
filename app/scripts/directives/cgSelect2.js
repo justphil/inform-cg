dc9cd88aa2776c2911e90a2644aea03ae4c16979
jkorrek <johannes.korrek@inform-software.com>
2014-06-24 11:41:13 +0200

    select2 control

--- app/scripts/directives/cgSelect2.js
+++ app/scripts/directives/cgSelect2.js
@@ -5,12 +5,25 @@ angular.module('informCgApp').directive('cgSelect2', function () {
     templateUrl : 'templates/directives/cgSelect2Template.html',
     link : function (scope) {
       console.log('cgSelect2 instance created');
-//TODO
+      var queryData = scope.dialog.dataModel[scope.data];
+      var select2Data = [];
+      for ( var i = 0; i < queryData.data.length; ++i) {
+        select2Data.push({
+          id : queryData.data[i][0],
+          text : queryData.data[i][0]
+        });
+      }
+
+      $('#' + scope.id).select2({
+        data : select2Data,
+        dropdownAutoWidth : true
+      });
     },
     scope : {
+      id : '@',
       value : '=',
       data : '@',
-      dialog: '='
+      dialog : '='
     }
   }
 
