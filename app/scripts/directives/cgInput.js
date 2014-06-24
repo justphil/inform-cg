279dcca50315939e3280accd8f57349e6c2451bd
syang <kugelfish@gmail.com>
2014-06-24 14:36:26 +0200

    add jqx binding for input element

--- app/scripts/directives/cgInput.js
+++ app/scripts/directives/cgInput.js
@@ -1,14 +1,15 @@
 angular.module('informCgApp').directive('cgInput', function() {
 
-  return {
-    restrict: 'E',
-    templateUrl: 'templates/directives/cgInputTemplate.html',
-    link: function() {
-      console.log('cgInput instance created');
-    },
-    scope:{
-      value:'='
+
+
+    console.log("ngJqxInput init function");
+
+    return {
+        restrict: 'E',
+        templateUrl: 'templates/directives/cgInputTemplate.html',
+        scope:{
+            value:'='
+        }
     }
-  }
 
 });
